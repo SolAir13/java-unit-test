ba79ebba2fa2eb96c061a8fe38402d7cfe569df0
Alexey Solomatin <asolomatin94@yandex.ru>
2023-11-04 12:20:01 +0300

    Add comment in Programm class

--- src/main/java/ru/yandex/praktikum/Program.java
+++ src/main/java/ru/yandex/praktikum/Program.java
@@ -2,15 +2,16 @@ package ru.yandex.praktikum;
 
 public class Program {
     public static void main(String[] args) {
-       Program age = new Program();
-       age.checkIsAdult(17);
+        Program age = new Program();
+        age.checkIsAdult(17);
 
     }
-    public  boolean checkIsAdult(int age){
-        if(age>=18){
+
+    public boolean checkIsAdult(int age) {
+        if (age >= 18) {
             return true;
         } else {
             return false;
         }
-    }
+    }//проверка возраста
 }

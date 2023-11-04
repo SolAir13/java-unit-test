83d4d9787ca3ac3aee96e27c6c8a916503cfbeef
Alexey Solomatin <asolomatin94@yandex.ru>
2023-11-04 10:50:58 +0300

    add the project

---
+++ src/main/java/ru/yandex/praktikum/Program.java
@@ -0,0 +1,16 @@
+package ru.yandex.praktikum;
+
+public class Program {
+    public static void main(String[] args) {
+       Program age = new Program();
+       age.checkIsAdult(17);
+
+    }
+    public  boolean checkIsAdult(int age){
+        if(age>=18){
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
